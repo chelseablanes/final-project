fd9f381cbd7da925015df7988edc145909c8143e
Emily Liu <liuemily@uw.edu>
2019-03-09 21:49:27 -0800

    load happiness csv

--- data.R
+++ data.R
@@ -2,7 +2,12 @@ library("dplyr")
 library("tidyr")
 library("shiny")
 
-unemployment <- read.csv('data/API_SL.UEM.TOTL.ZS_DS2_en_csv_v2_10473697.csv', stringsAsFactors = FALSE)
+# load unemployment csv
+unemployment <- read.csv('data/API_SL.UEM.TOTL.ZS_DS2_en_csv_v2_10473697.csv', 
+                         stringsAsFactors = FALSE)
 unemployment <- unemployment %>% 
   select(Country.Name, Country.Code, X2016)
 View(unemployment)
+
+# load happiness csv
+happiness <- read.csv('data/2016.csv', stringsAsFactors = FALSE)
