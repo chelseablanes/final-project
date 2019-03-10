877fd6e21bf012509dabec3ea18ff2d2ae686629
Emily Liu <liuemily@uw.edu>
2019-03-09 21:45:58 -0800

    add library and selected 2016 columns for unemployment data frame

--- data.R
+++ data.R
@@ -1,3 +1,8 @@
+library("dplyr")
+library("tidyr")
+library("shiny")
 
 unemployment <- read.csv('data/API_SL.UEM.TOTL.ZS_DS2_en_csv_v2_10473697.csv', stringsAsFactors = FALSE)
+unemployment <- unemployment %>% 
+  select(Country.Name, Country.Code, X2016)
 View(unemployment)
